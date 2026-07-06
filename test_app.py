2832c24eb8319e0e79777337f408663c3d79dc4f
Marco Solano <solanohma@gmail.com>
2026-07-06 16:38:26 -0600

    changes on pipeline

--- test_app.py
+++ test_app.py
@@ -7,7 +7,16 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_home_page(client):
-    """Verifica que la ruta principal cargue correctamente"""
+def test_home_page_status(client):
+    """Verifica que la ruta principal responda exitosamente"""
     respuesta = client.get('/')
     assert respuesta.status_code == 200
+
+def test_home_page_content(client):
+    """Verifica que el HTML renderizado contenga los elementos clave de la app"""
+    respuesta = client.get('/')
+    html_contenido = respuesta.data.decode('utf-8')
+    
+    # Validamos que las palabras clave de tu dashboard de psutil existan en el HTML
+    assert "CPU" in html_contenido
+    assert "Memoria" in html_contenido
